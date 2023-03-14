83872b7c4ca48f89bf19b3f045619e5e741725bc
Vlada <Guzeieiva.Vlada@lll.kpi.ua>
2023-03-15 00:36:49 +0300

    initial commit

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApplication4
+{
+    using System;
+    using System.Collections.Generic;
+    
+    internal class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("1 Завдання");
+            List<string> rowsofverse = new List<string>();
+          
+            rowsofverse.Add("Little leaves fall softly down");
+            rowsofverse.Add("Red and yellow, orange and brown");
+            rowsofverse.Add("Whirling, twirling round and round");
+            rowsofverse.Add("Falling softly to the ground");
+            rowsofverse.Add("Little leaves fall softly down");
+            rowsofverse.Add("To make a carpet on the ground");
+            rowsofverse.Add("Then, swish, the wind comes whistling by");
+            rowsofverse.Add("And sends them dancing to the sky");
+          
+            rowsofverse.Sort((x, y) => x.Length.CompareTo(y.Length)) ;
+          
+            foreach (var rows in rowsofverse)
+            {
+                Console.WriteLine(rows);
+            }
+        } 
+    }
+}
